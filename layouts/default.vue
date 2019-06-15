5b90501cb263d66574c5a17f71d28211874edd1e
Jae Lee <jaeslee@utexas.edu>
2019-06-15 07:48:15 -0500

    Vue.js change.

---
+++ layouts/default.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-app>
+        <v-toolbar fixed app class="px-5" id="mainNav">
+            <v-toolbar-title v-text="title"></v-toolbar-title>
+            <v-spacer />
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn flat v-for="link in links" :key="link.name" :to="link.route" id="navItem">
+                    {{ link.name }}
+                </v-btn>
+            </v-toolbar-items>
+
+            <v-menu light class="hidden-md-and-up">
+                <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+                <v-list>
+                    <v-list-tile v-for="link in links" :key="link.name" :to="link.route">
+                         <v-list-tile-content>
+                            <v-list-tile-title>{{ link.name }}</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>   
+                </v-list>
+            </v-menu>
+        </v-toolbar>
+        <v-content class="pa-0">
+            <nuxt />
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'SC Construction',
+            links: [
+                { name: 'Home', route: '/', id: 'navItem0' },
+                { name: 'About', route: '/about', id: 'navItem1' },
+                { name: 'Projects', route: '/projects', id: 'navItem2' },
+                { name: 'Contact', route: '/contact', id: 'navItem3' },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+#mainNav {
+    background-color: rgba(255, 255, 255, 0.95);
+}
+
+#navItem:hover {
+    color: #AF623A;
+}
+
+#navItem.v-btn--active {
+    color: #AF623A;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-container fluid class="pa-0 ma-0" id="hero_container">
+        <!-- Photo by Verne Ho on Unsplash -->
+        <v-parallax :src="require('@/assets/hero.jpg')" height="/* */" id="hero_image">
+            <v-layout align-center justify-center class="text-xs-center">
+                <v-card white class="mx-5 elevation-24" id="hero_card">
+                    <h1 class="display-2 font-weight-black ma-4 black--text">BUILDING TRUST IN BUSINESS RELATIONSHIPS</h1>
+                </v-card>
+            </v-layout>
+        </v-parallax>
+    </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+#hero_container {
+    height: 100%;
+}
+
+#hero_image {
+    height: 100%;
+}
+
+.v-parallax__image {
+    bottom: -100px;
+}
+
+#hero_card {
+    background-color: rgba(200, 200, 200, 0.3);
+    border-radius: 15px;
+}
+</style>
+
